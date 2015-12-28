4d6843bab591a532e1d31d6a3c1a0388be4180f1
Hisanari Otsu <hi2p.perim@gmail.com>
2015-12-28 18:42:52 +0900

    WIP: Asset loading

---
+++ include/lightmetrica/trianglemesh.h
@@ -0,0 +1,53 @@
+/*
+    Lightmetrica - A modern, research-oriented renderer
+
+    Copyright (c) 2015 Hisanari Otsu
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <lightmetrica/asset.h>
+
+LM_NAMESPACE_BEGIN
+
+class TriangleMesh : public Asset
+{
+private:
+
+    LM_INTERFACE_CLASS(TriangleMesh, Asset);
+
+public:
+
+    TriangleMesh() = default;
+    LM_DISABLE_COPY_AND_MOVE(TriangleMesh);
+
+public:
+
+    LM_INTERFACE_F(NumVertices, int());
+    LM_INTERFACE_F(NumFaces, int());
+    LM_INTERFACE_F(Positions, const double*());
+    LM_INTERFACE_F(Normals, const double*());
+    LM_INTERFACE_F(Texcoords, const double*());
+    LM_INTERFACE_F(Faces, const unsigned int*());
+
+};
+
+LM_NAMESPACE_END
