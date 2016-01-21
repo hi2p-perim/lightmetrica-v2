1e7d3b7d9eba81bb28f923b18f9fbe71ef272003
Hisanari Otsu <hi2p.perim@gmail.com>
2016-01-21 21:43:35 +0900

    WIP: renderer, sampling, etc.

---
+++ src/liblightmetrica/renderer/renderer_null.cpp
@@ -0,0 +1,65 @@
+/*
+    Lightmetrica - A modern, research-oriented renderer
+
+    Copyright (c) 2015 Hisanari Otsu
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+#include <lightmetrica/renderer.h>
+#include <lightmetrica/scene.h>
+#include <lightmetrica/film.h>
+#include <lightmetrica/property.h>
+
+LM_NAMESPACE_BEGIN
+
+class Renderer_Null : public Renderer
+{
+public:
+
+    LM_IMPL_CLASS(Renderer_Null, Renderer);
+
+public:
+
+    LM_IMPL_F(Initialize) = [this](const PropertyNode* prop) -> bool
+    {
+        return true;
+    };
+
+    LM_IMPL_F(Render) = [this](const Scene* scene, Film* film) -> void
+    {
+        // Do nothing. Just output blank image.
+        for (int y = 0; y < film->Height(); y++)
+        {
+            for (int x = 0; x < film->Width(); x++)
+            {
+                film->SetPixel(x, y, SPD());
+            }
+
+            const double progress = 100.0 * y / film->Height();
+            LM_LOG_INPLACE(boost::str(boost::format("Progress: %.1f%%") % progress));
+        }
+        LM_LOG_INFO("Progress: 100.0%");
+    };
+
+};
+
+LM_COMPONENT_REGISTER_IMPL(Renderer_Null, "nullrenderer");
+
+LM_NAMESPACE_END
---
+++ src/lightmetrica-test/test_film.cpp
@@ -0,0 +1,66 @@
+/*
+    Lightmetrica - A modern, research-oriented renderer
+
+    Copyright (c) 2015 Hisanari Otsu
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+#include <pch_test.h>
+#include <lightmetrica/film.h>
+#include <lightmetrica/property.h>
+#include <lightmetrica/logger.h>
+#include <lightmetrica-test/utils.h>
+
+LM_TEST_NAMESPACE_BEGIN
+
+#pragma region Fixture
+
+struct FilmTest : public ::testing::TestWithParam<const char*>
+{
+    virtual auto SetUp() -> void override { Logger::SetVerboseLevel(2); Logger::Run(); }
+    virtual auto TearDown() -> void override { Logger::Stop(); }
+};
+
+INSTANTIATE_TEST_CASE_P(FilmTypes, FilmTest, ::testing::Values("hdr"));
+
+#pragma endregion
+
+// --------------------------------------------------------------------------------
+
+#pragma region Tests
+
+TEST_P(FilmTest, Initialize)
+{
+    const auto prop = ComponentFactory::Create<PropertyTree>();
+    ASSERT_TRUE(prop->LoadFromString(TestUtils::MultiLineLiteral(R"x(
+    | w: 1000
+    | h: 500
+    )x")));
+
+    const auto film = ComponentFactory::Create<Film>(GetParam());
+    EXPECT_TRUE(film->Initialize(prop->Root()));
+
+    EXPECT_EQ(1000, film->Width());
+    EXPECT_EQ(500, film->Height());
+}
+
+#pragma endregion
+
+LM_TEST_NAMESPACE_END
