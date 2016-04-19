ee77b7cd3d00ed516d2b83ee763a441153d63dfc
Hisanari Otsu <hi2p.perim@gmail.com>
2016-04-19 12:21:08 +0900

    WIP: Implementing PPM

--- src/liblightmetrica/renderer/renderer_ppm.cpp
+++ src/liblightmetrica/renderer/renderer_ppm.cpp
@@ -26,23 +26,26 @@
 #include <lightmetrica/renderer.h>
 #include <lightmetrica/property.h>
 #include <lightmetrica/random.h>
+#include <lightmetrica/scene.h>
+#include <lightmetrica/film.h>
+#include <lightmetrica/bsdf.h>
+#include <lightmetrica/ray.h>
+#include <lightmetrica/intersection.h>
+#include <lightmetrica/emitter.h>
+#include <lightmetrica/sensor.h>
+#include <lightmetrica/surfacegeometry.h>
+#include <lightmetrica/primitive.h>
+#include <lightmetrica/scheduler.h>
+#include <lightmetrica/renderutils.h>
 
 LM_NAMESPACE_BEGIN
 
-#pragma region Importance map
-
-struct HitPoint
-{
-    
-};
-
-#pragma endregion
-
-// --------------------------------------------------------------------------------
-
 /*!
     \brief Progressive photon mapping renderer.
-    Implements progressive photon mapping.
+
+    - Implements progressive photon mapping
+    - Based on the simplified implementation by Toshiya Hachisuka
+      http://www.ci.i.u-tokyo.ac.jp/~hachisuka/smallppm_exp.cpp
 */
 class Renderer_PPM final : public Renderer
 {
@@ -72,28 +75,35 @@ public:
 
         #pragma region Function to trace subpath
 
-        const auto TraceSubpath = [&](Random* rng, TransportDirection transDir, std::function<void()>& surfaceInteractionFunc) -> void
+        struct PathVertex
+        {
+            int type;
+            SurfaceGeometry geom;
+            const Primitive* primitive = nullptr;
+        };
+
+        const auto TraceSubpath = [this, &scene](Random* rng, TransportDirection transDir, const std::function<bool(const PathVertex&)>& processPathVertexFunc) -> void
         {
             Vec3 initWo;
-            for (int step = 0; maxPathVertices == -1 || step < maxPathVertices; step++)
+            PathVertex pv, ppv;
+            SPD throughput(1_f);
+            for (int step = 0; maxNumVertices_ == -1 || step < maxNumVertices_; step++)
             {
                 if (step == 0)
                 {
                     #pragma region Sample initial vertex
 
-                    PathVertex sv;
+                    PathVertex v;
 
                     // Sample an emitter
-                    sv.type = transDir == TransportDirection::LE ? SurfaceInteractionType::L : SurfaceInteractionType::E;
-                    sv.primitive = scene->SampleEmitter(sv.type, rng->Next());
+                    v.type = transDir == TransportDirection::LE ? SurfaceInteractionType::L : SurfaceInteractionType::E;
+                    v.primitive = scene->SampleEmitter(v.type, rng->Next());
 
                     // Sample a position on the emitter and initial ray direction
-                    sv.primitive->emitter->SamplePositionAndDirection(rng->Next2D(), rng->Next2D(), sv.geom, initWo);
+                    v.primitive->emitter->SamplePositionAndDirection(rng->Next2D(), rng->Next2D(), v.geom, initWo);
 
                     // Add a vertex
-                    SubpathVertex v;
-                    v.sv = sv;
-                    vertices.push_back(v);
+                    pv = v;
 
                     #pragma endregion
                 }
@@ -101,85 +111,51 @@ public:
                 {
                     #pragma region Sample a vertex with PDF with BSDF
 
-                    const auto sv = [&]() -> boost::optional<PathVertex>
+                    // Sample a next direction
+                    Vec3 wi;
+                    Vec3 wo;
+                    if (step == 1)
                     {
-                        // Previous & two before vertex
-                        const auto* pv = &vertices.back().sv.get();
-                        const auto* ppv = vertices.size() > 1 ? &vertices[vertices.size() - 2].sv.get() : nullptr;
+                        wi = Vec3();
+                        wo = initWo;
+                    }
+                    else
+                    {
+                        wi = Math::Normalize(ppv.geom.p - pv.geom.p);
+                        pv.primitive->surface->SampleDirection(rng->Next2D(), rng->Next(), pv.type, pv.geom, wi, wo);
+                    }
+                    const auto f = pv.primitive->surface->EvaluateDirection(pv.geom, pv.type, wi, wo, transDir, false);
+                    if (f.Black())
+                    {
+                        break;
+                    }
 
-                        // Sample a next direction
-                        Vec3 wo;
-                        const auto wi = ppv ? Math::Normalize(ppv->geom.p - pv->geom.p) : Vec3();
-                        if (step == 1)
-                        {
-                            wo = initWo;
-                        }
-                        else
-                        {
-                            pv->primitive->surface->SampleDirection(rng->Next2D(), rng->Next(), pv->type, pv->geom, wi, wo);
-                        }
-                        const auto f = pv->primitive->surface->EvaluateDirection(pv->geom, pv->type, wi, wo, transDir, false);
-                        if (f.Black())
-                        {
-                            return boost::none;
-                        }
+                    // Intersection query
+                    Ray ray = { pv.geom.p, wo };
+                    Intersection isect;
+                    if (!scene->Intersect(ray, isect))
+                    {
+                        break;
+                    }
 
-                        // Intersection query
-                        Ray ray = { pv->geom.p, wo };
-                        Intersection isect;
-                        if (!scene->Intersect(ray, isect))
-                        {
-                            return boost::none;
-                        }
-
-                        // Create vertex
-                        PathVertex v;
-                        v.geom = isect.geom;
-                        v.primitive = isect.primitive;
-                        v.type = isect.primitive->surface->Type() & ~SurfaceInteractionType::Emitter;
-
-                        return v;
-                    }();
+                    if (isect.geom.infinite)
+                    {
+                        break;
+                    }
 
                     #pragma endregion
 
                     // --------------------------------------------------------------------------------
 
-                    #pragma region Sample a vertex with direct emitter sampling
+                    #pragma region Process path vertex
 
-                    const auto direct = [&]() -> boost::optional<PathVertex>
+                    PathVertex v;
+                    v.geom = isect.geom;
+                    v.primitive = isect.primitive;
+                    v.type = isect.primitive->surface->Type() & ~SurfaceInteractionType::Emitter;
+                    if (!processPathVertexFunc(v))
                     {
-                        PathVertex v;
-                        const auto& pv = vertices.back().sv;
-
-                        // Sample a emitter
-                        v.type = transDir == TransportDirection::LE ? SurfaceInteractionType::E : SurfaceInteractionType::L;
-                        v.primitive = scene->SampleEmitter(v.type, rng->Next());
-
-                        // Sample a position on the emitter
-                        v.primitive->emitter->SamplePositionGivenPreviousPosition(rng->Next2D(), pv->geom, v.geom);
-
-                        // Check visibility
-                        if (!scene->Visible(pv->geom.p, v.geom.p))
-                        {
-                            return boost::none;
-                        }
-
-                        return v;
-                    }();
-
-                    #pragma endregion
-
-                    // --------------------------------------------------------------------------------
-
-                    #pragma region Add a vertex
-
-                    if (sv || direct)
-                    {
-                        SubpathVertex v;
-                        v.sv = sv;
-                        v.direct = direct;
-                        vertices.push_back(v);
+                        break;
                     }
 
                     #pragma endregion
@@ -188,22 +164,21 @@ public:
 
                     #pragma region Path termination
 
-                    if (!sv)
-                    {
-                        break;
-                    }
-
-                    if (sv->geom.infinite)
-                    {
-                        break;
-                    }
-
                     // TODO: replace it with efficient one
                     const Float rrProb = 0.5_f;
                     if (rng->Next() > rrProb)
                     {
                         break;
                     }
+
+                    #pragma endregion
+
+                    // --------------------------------------------------------------------------------
+
+                    #pragma region Update information
+
+                    ppv = pv;
+                    pv = v;
 
                     #pragma endregion
                 }
@@ -216,13 +191,42 @@ public:
 
         #pragma region Collect measumrement points
 
+        std::vector<PathVertex> mp;
+
         for (long long sample = 0; sample < numSamples_; sample++)
         {
-            
-        }
+            TraceSubpath(&initRng, TransportDirection::EL, [&](const PathVertex& v) -> bool
+            {
+                if ((v.type & SurfaceInteractionType::D) > 0 || (v.type & SurfaceInteractionType::G) > 0)
+                {
+                    // Record the measurement point, and terminate the path
+                    mp.push_back(v);
+                    return false;
+                }
 
+                // If the intersected point is specular surface, continue to trace the path
+                return true;
+            });
+        }
+        
         #pragma endregion
 
+        // --------------------------------------------------------------------------------
+
+#if 1
+
+        // Output the measurement points to the file
+        {
+            std::ofstream ofs("mp.dat");
+            for (const auto& v : mp)
+            {
+                const auto& p = v.geom.p;
+                ofs << boost::str(boost::format("%.15f %.15f %.15f") % p.x % p.y % p.z) << std::endl;
+            }
+        }
+
+#endif
+        
         // --------------------------------------------------------------------------------
 
         #pragma region Create acceleration structure from a set of measurement points
