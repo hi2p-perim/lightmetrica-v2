4e20f428903df6fef5e1be607ba19315974109b6
Hisanari Otsu <hi2p.perim@gmail.com>
2016-06-16 09:22:23 +0900

    Fixed a build script for osx environment

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-	cd $TRAVIS_BUILD_DIR/dist/build/bin/Release
+	cd $TRAVIS_BUILD_DIR/dist/bin/Release
 	./lightmetrica
 else
 	docker run lightmetrica
